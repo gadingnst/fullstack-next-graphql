18bdbacfc545929c42a29ad06d4f57010ba53641
Sutan Nasution <sutan.gnst@gmail.com>
2020-02-20 17:49:34 +0700

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,12 +2,12 @@ export const isObject = object => object && typeof object === 'object'
 
 export const deepMerge = (...object) => object.reduce((acc, cur) => {
     Object.keys(cur).forEach(key => {
-        acc[key] = isObject(acc[key]) && isObject(cur[key])
-            ? deepMerge(acc[key], cur[key])
-            : Array.isArray(acc[key]) && Array.isArray(cur[key])
+        acc[key] = Array.isArray(acc[key]) && Array.isArray(cur[key])
                 ? [...acc[key], ...cur[key]]
                     .filter((value, idx, arr) => arr.indexOf(value) === idx)
-                : cur[key]
+                : isObject(acc[key]) && isObject(cur[key])
+                    ? deepMerge(acc[key], cur[key])
+                    : cur[key]
     })
     return acc
 }, {})
